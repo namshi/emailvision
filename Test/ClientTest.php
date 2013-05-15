28f7658bbd458e2ca68f5592dab30749f1bb2e3b
Alessandro Nadalin <alessandro.nadalin@gmail.com>
2013-05-16 00:04:10 +0400

    Added a blank test and configured phpunit

---
+++ Test/ClientTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Namshi\Emailvision\Test;
+
+use \PHPUnit_Framework_TestCase;
+
+class ClientTest extends PHPUnit_Framework_TestCase
+{
+    public function testCreationOfTheClient()
+    {
+        
+    }
+}
